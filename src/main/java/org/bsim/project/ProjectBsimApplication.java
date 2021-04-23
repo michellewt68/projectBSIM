1a76a240c71bdeea26e6e5f6284b0e25957aec11
michellewt68 <michellewt68@gmail.com>
2021-04-23 12:08:10 +0700

    initialized application project

---
+++ src/main/java/org/bsim/project/ProjectBsimApplication.java
@@ -0,0 +1,13 @@
+package org.bsim.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBsimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBsimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bsim/project/ProjectBsimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsim.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBsimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
